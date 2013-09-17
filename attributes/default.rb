7017cd089b6f3ce1d1764683a0f8d29facf0d6ac
Jean-Baptiste Note <jb.note@criteo.com>
2013-09-17 18:41:37 +0200

    FileCreateMode can now be adjusted

--- attributes/default.rb
+++ attributes/default.rb
@@ -30,6 +30,7 @@ default["rsyslog"]["per_host_dir"]              = "%$YEAR%/%$MONTH%/%$DAY%/%HOST
 default["rsyslog"]["max_message_size"]          = "2k"
 default["rsyslog"]["preserve_fqdn"]             = "off"
 default["rsyslog"]["high_precision_timestamps"] = false
+default["rsyslog"]["filecreatemode"]            = "0640"
 
 # The most likely platform-specific attributes
 default["rsyslog"]["service_name"]     = "rsyslog"
